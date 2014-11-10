0066137f8f14bbaebe94991c17d3e7024ecdeaf0
YannickDot <yannick.levif@gmail.com>
2014-11-10 01:51:07 +0100

    Created project with working configuration of Google Maps API

---
+++ sources/gen/com/enseirb/swissknife33/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.enseirb.swissknife33;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ sources/src/com/enseirb/swissknife33/MainActivity.java
@@ -0,0 +1,15 @@
+package com.enseirb.swissknife33;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+
+}
