b4057006c9069cc9a41d1e9148fb41519f8bab1f
blondeausarah <sarah_blondeau@hotmail.com>
2014-11-17 15:21:47 +0100

    Adding DTO classes

---
+++ sources/src/com/enseirb/swissknife33/dao/model/NestDTO.java
@@ -0,0 +1,29 @@
+package com.enseirb.swissknife33.dao.model;
+
+public class NestDTO {
+	private String cle;
+	private String nature_abris;
+	private String detail_espece_visee;
+	private String nom;
+	private String x_long;
+	private String y_lat;
+	public void setCle(String cle) {
+		this.cle = cle;
+	}
+	public void setNature_abris(String nature_abris) {
+		this.nature_abris = nature_abris;
+	}
+	public void setDetail_espece_visee(String detail_espece_visee) {
+		this.detail_espece_visee = detail_espece_visee;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setX_long(String x_long) {
+		this.x_long = x_long;
+	}
+	public void setY_lat(String y_lat) {
+		this.y_lat = y_lat;
+	}
+	
+}
---
+++ sources/src/com/enseirb/swissknife33/dao/model/ToiletsDTO.java
@@ -0,0 +1,33 @@
+package com.enseirb.swissknife33.dao.model;
+
+public class ToiletsDTO {
+	private String cle;
+	private String adresse;
+	private String nom;
+	private String quartier;
+	private String typologie;
+	private String x_long;
+	private String y_lat;
+	public void setCle(String cle) {
+		this.cle = cle;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setQuartier(String quartier) {
+		this.quartier = quartier;
+	}
+	public void setTypologie(String typologie) {
+		this.typologie = typologie;
+	}
+	public void setX_long(String x_long) {
+		this.x_long = x_long;
+	}
+	public void setY_lat(String y_lat) {
+		this.y_lat = y_lat;
+	}
+	
+}
